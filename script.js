b8b86c944ecf7910dceb2f68ea455b5e38ef6c97
Kishan-Borad <kishan.borad@quantiphi.com>
2019-06-07 19:06:53 +0530

    Added Digital watch

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ console.log("script")
 var diallines = document.getElementsByClassName('diallines');
 var clockE1 = document.getElementsByClassName('clock')[0];
 
-console.log(diallines);
+//Clock minutes dial lines
 for(var i=1; i < 60; i++)
 {
     clockE1.innerHTML += "<div class='diallines'></div>"
     diallines[i].style.transform = "rotate(" + 6 * i +"deg)";
 }
-
+// Clock Funtion
 function clock(){
     var weekday = new Array(7),
     d = new Date(),
@@ -20,6 +20,7 @@ function clock(){
     month = d.getMonth() + 1,
     year = d.getYear(),
 
+    //Clock hand moment
     hDeg = h * 30 + m * (360/720),
     mDeg = m * 6 + s * (360/3600),
     sDeg = s * 6,
@@ -30,7 +31,7 @@ function clock(){
     dateE1 = document.querySelector('.date'),
     dayE1 = document.querySelector('.day');
 
-
+    //Week days 
     weekday[0] = "Sunday";
     weekday[1] = "Monday";
     weekday[2] = "Tuesday";
@@ -43,9 +44,35 @@ function clock(){
     hE1.style.transform = "rotate("+hDeg+"deg)";
     mE1.style.transform = "rotate("+mDeg+"deg)";
     sE1.style.transform = "rotate("+sDeg+"deg)";
-    dateE1.innerHTML = date+"/"+month+"/"+year;
-    dayE1.innerHTML = day;
+    dateE1.innerHTML = date+"/"+month+"/"+year; //Date format 
+    dayE1.innerHTML = day; //Day format
 
 }
 
 setInterval("clock()", 100);
+
+
+$(document).ready(function() {
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    
+    
+    var newDate = new Date();
+    newDate.setDate(newDate.getDate());
+    $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+    
+    setInterval( function() {
+        var seconds = new Date().getSeconds();
+        $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+        },1000);
+        
+    setInterval( function() {
+        var minutes = new Date().getMinutes();
+        $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+        },1000);
+        
+    setInterval( function() {
+        var hours = new Date().getHours();
+        $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+        }, 1000);	
+    });
